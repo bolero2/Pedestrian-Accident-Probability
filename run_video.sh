5313909ac24ca881a7345bfe5bd928aa9950044f
Dae-Cheol Noh <sheocjf1025@gmail.com>
2022-04-12 21:34:08 +0900

    Update run_video.sh

--- run_video.sh
+++ run_video.sh
@@ -1,1 +1,1 @@
-python /home/bolero/dl/hrnet/simple-HigherHRNet/scripts/live-demo.py -c 48 -w ./weights/pose_higher_hrnet_w48_640.pth -r 512 --filename ./clip9.mp4 --save_video
+python /home/bolero/dl/hrnet/simple-HigherHRNet/scripts/live-demo.py -c 32 -w ./weights/pose_higher_hrnet_w32_640.pth -r 512 --filename ./clip9.mp4 --save_video
