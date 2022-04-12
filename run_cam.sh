1df2ee13425f8484fea0d6bff8699ab28c572191
Dae-Cheol Noh <sheocjf1025@gmail.com>
2022-04-12 21:33:46 +0900

    Update run_cam.sh

--- run_cam.sh
+++ run_cam.sh
@@ -1,1 +1,1 @@
-python /home/bolero/dl/hrnet/simple-HigherHRNet/scripts/live-demo.py -c 48 -w ./weights/pose_higher_hrnet_w48_640.pth -r 512 --camera_id 0
+python /home/bolero/dl/hrnet/simple-HigherHRNet/scripts/live-demo.py -c 32 -w ./weights/pose_higher_hrnet_w32_640.pth -r 512 --camera_id 0
